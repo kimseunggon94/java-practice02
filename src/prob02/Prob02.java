79f7b8d5b1531221362f7aa8366e871d5cff178c
BIT <BIT@DESKTOP-TJ8UB7F>
2019-08-07 16:11:15 +0900

    문제2

--- src/prob02/Prob02.java
+++ src/prob02/Prob02.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class Prob02 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("5개의 숫자를 입력하세요.");
+		int intArray[] = new int[5];
+		double sum=0, ave;
+		for(int i=0;i<intArray.length;i++) {
+			intArray[i]=sc.nextInt();
+		}
+		for(int j=0;j<intArray.length;j++) {
+			sum +=	intArray[j];
+		}
+		ave=sum/intArray.length;
+		
+		System.out.println("평균은 "+ave+" 입니다.");
 
-		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
-		System.out.println( "5개의 숫자를 입력하세요." );
-		
-		/* 배열에 저장된 정수 값 더하기 */
-		
-		/* 출력 */
-		
-		/* 자원정리 */
-		scanner.close();
+		sc.close();
 	}
 }
